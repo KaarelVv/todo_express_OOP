b0f7f80818962feea9015bd1b480cc29b1770d38
Kaarel <kaarel.viilvere@voco.ee>
2024-11-29 14:54:31 +0200

    Create project basic files

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+
+const app = express()
+app.use(bodyParser.json())
+
+
+app.get('/json-test', (req,res) => {
+    res.send({
+        message: 'json text ok'
+    } )
+} )
+app.listen(3009, () => {
+    console.log('server is connected at port 3009')
+})
