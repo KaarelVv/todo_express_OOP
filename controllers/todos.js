15811086942f04a10a39392cb29bfc4a808d1b66
Kaarel <viilvere.k@gmail.com>
2024-11-30 09:35:39 +0200

    Delete the Todo by id

--- controllers/todos.js
+++ controllers/todos.js
@@ -27,31 +27,35 @@ class todoController {
         const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
         if (todoIndex < 0) {
             throw new Error('Could not find id'),
-            res.json({ message: 'Could not find task with this index'})
+            res.json({ message: 'Could not find task with this index' })
 
         }
-        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask ) 
+        this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
 
-        res.json({message: 'Updated todo', updatedTask: this.TODOS[todoIndex] }
-            
-         )
+        res.json({ message: 'Updated todo', updatedTask: this.TODOS[todoIndex] }
+
+        )
     }
 
-    deleteTodo(req, res){
+    deleteTodo(req, res) {
         const todoId = req.params.id
-        const task = req.body.task
+
         const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
         if (todoIndex < 0) {
             throw new Error('Could not find id'),
             res.json({ message: 'Could not find task with this index' })
+        }
 
-    }  
-    this.TODOS.pop()
-    // use splice()
-    res.json({message: 'Deleted' }
-            
-    )
-} 
+        this.TODOS.splice(todoIndex, 1)
+        
+
+
+
+        res.json({ message: 'Deleted task at index ' + todoIndex }
+
+        )
+    }
 }
 
 
